28fceee0ab0a9a5028e31393a5df0fbd7b6e85ec
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-19 14:05:32 -0800

    code cleanup

--- parseSpringerweb.py
+++ parseSpringerweb.py
@@ -1,7 +1,5 @@
-from bs4 import BeautifulSoup
 import pymongo
-import requests
-from lxml import html
+from bs4 import BeautifulSoup
 
 client = pymongo.MongoClient()
 db = client.springer
@@ -11,13 +9,13 @@ if __name__ == '__main__':
     for doc in db['pauling_file_unique'].find({'key': 'sd_1213731'}).limit(1):
         db['pauling_file_WebParse'].insert(doc)
     for doc in db['pauling_file_WebParse'].find():
-        # soup = BeautifulSoup(doc['webpage_str'], 'lxml')
-        # geninfo = soup.find('div', {'id': 'experimentalDetails'}).find('div', 'accordion__bd')
-        # tables = geninfo.findAll('table')
-        # for table in tables:
-        #     trs = table.findAll('tr')
-        #     results = {tr.findAll('td')[0].string.strip(): tr.findAll('td')[1].find('ul').find('li') for tr in trs}
-        #     print results
+        soup = BeautifulSoup(doc['webpage_str'], 'lxml')
+        geninfo = soup.find('div', {'id': 'experimentalDetails'}).find('div', 'accordion__bd')
+        tables = geninfo.findAll('table')
+        for table in tables:
+            trs = table.findAll('tr')
+            results = {tr.findAll('td')[0].string.strip(): tr.findAll('td')[1].find('ul').find('li') for tr in trs}
+            print results
 
         # print ''.join([(str(item.encode('utf-8'))).strip() for item in ref.contents])
 
@@ -28,23 +26,11 @@ if __name__ == '__main__':
         # print StructureNL(cif_struct, data=data_dict,
         #                   authors=['Saurabh Bajaj <sbajaj@lbl.gov>', 'Anubhav Jain <ajain@lbl.gov>'])//div[@id="general_information"]
 
-        parsed_body = html.fromstring(doc['webpage_str'])
-        y = 0
-        for i in parsed_body.xpath('//div[@class="general_information"]/div/div/div/ul/li/strong[@class="data-list__item-key"]/text()'):
-            y += 1
-            field = i.strip()
-            if field.endswith(':'):
-                print field[:-1]
-            else:
-                print field
-        print 'Total # of keys  = {}'.format(y)
-        # print len(parsed_body.xpath('//strong[@class="data-list__item-key"]/text()'))
-        # print len(parsed_body.xpath('//span[@class="data-list__item-value"]/text()'))
-        x = 0
-        for i in parsed_body.xpath('//span[@class="data-list__item-value"]'):
-            x += 1
-            print i.text_content().strip()
-        print 'Total # of values  = {}'.format(x)
+        # x = 0
+        # for i in parsed_body.xpath('//span[@class="data-list__item-value"]'):
+        #     x += 1
+        #     print i.text_content().strip()
+        # print 'Total # of values  = {}'.format(x)
 
         # for a_link in parsed_body.xpath('//a/@href'):
         #     if '.cif' in a_link:
