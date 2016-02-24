544ec33f9d9811959933af5188611fabb25fc69f
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-24 10:07:38 -0800

    handle exceptions in density parsing

--- densityParse.py
+++ densityParse.py
@@ -10,6 +10,10 @@ db = client.springer
 if __name__ == '__main__':
     d = 0
     for doc in db['pauling_file_unique_Parse'].find().sort('_id', pymongo.ASCENDING).limit(100):
-        density = doc['metadata']['_Springer']['geninfo']['Density']
-        print doc['key'], density
-        print float(density.split()[2])
+        try:
+            density = doc['metadata']['_Springer']['geninfo']['Density']
+            print doc['key'], density
+            print float(density.split()[2])
+        except Exception as e:
+            print 'Exception at {}'.format(doc['key'])
+            print 'Density given as ' + doc['metadata']['_Springer']['geninfo']['Density']
