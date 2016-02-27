1e90348d8030ee4db00ba70a7d3e03e58ecbca93
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-27 09:09:00 -0800

    modulating function for handling partial occupancies (without brackets)

--- cifEditor.py
+++ cifEditor.py
@@ -10,7 +10,7 @@ db = client.springer
 
 def handle_insufficientpowderdata(cif_string):
     """
-    Handles CIF errors arising from too few or too many values in data loop for diffraction data.
+    Handles CIF parsing errors arising from too few or too many values in data loop for diffraction data.
 
     :param cif_string: (str) cif file
     :return: corrected cif string
@@ -61,6 +61,44 @@ def handle_insufficientpowderdata(cif_string):
         except AssertionError:
             print 'UNSUCCESSFUL 2nd attempt (uncommenting all lines with insufficient or too many values for powder ' \
                   'pattern)'
+
+
+def handle_partialocclables(cif_string):
+    """
+    Handles CIF parsing errors arising from atom labels containing partial occupancy numbers
+
+    :param cif_string: (str) cif file
+    :return: corrected cif string
+    """
+    cif_lines = json.loads(json.dumps(cif_string)).splitlines()
+    cif_string_new = ''
+    for line in cif_lines:
+        if ' + ' in line:
+            matching_list = re.findall(r'\'(.+?)\'', line)
+            elemocc = matching_list[0].split('+')
+            elems = []
+            occupancies = []
+            for i in range(len(elemocc)):
+                occupancies.append('0' + re.findall('\.?\d+', elemocc[i].strip())[1])
+                c = re.findall('\D+', elemocc[i].strip())
+                elems.append(c[1])
+            newline = '#' + line
+            cif_string_new += newline + '\n'
+            for i in range(len(elems)):
+                oldline = line
+                old_elemline = oldline.replace("'" + matching_list[0] + "'", "'" + elems[i] + "'")
+                new_elemline_list = old_elemline.split()
+                new_elemline_list[7] = occupancies[i]
+                new_elemline_list.append('\n')
+                new_elemline = ' '.join(new_elemline_list)
+                cif_string_new += new_elemline
+        else:
+            cif_string_new += line + '\n'
+    try:
+        print CifParser.from_string(cif_string_new).get_structures()[0].as_dict()
+        return cif_string_new
+    except:
+        print 'UNSUCCESSFUL - Could not correct partial occupancy numbers (without brackets)'
 
 
 if __name__ == '__main__':
@@ -148,49 +186,5 @@ if __name__ == '__main__':
                 print('! Could not parse structure for: {}'.format(doc['key']))
                 print(traceback.format_exc())
                 print 'Now trying to modify cif string'
-                cif_string_new = ''
-                try:
-                    for line in (json.loads(json.dumps(doc['cif_string']))).splitlines():
-                        if ' + ' in line:
-                            matching_list = re.findall(r'\'(.+?)\'', line)
-                            elemocc = matching_list[0].split('+')
-                            elems = []
-                            occupancies = []
-                            for i in range(len(elemocc)):
-                                occupancies.append('0' + re.findall('\.?\d+', elemocc[i].strip())[1])
-                                c = re.findall('\D+', elemocc[i].strip())
-                                elems.append(c[1])
-                            newline = '#' + line
-                            cif_string_new += newline + '\n'
-                            for i in range(len(elems)):
-                                oldline = line
-                                old_elemline = oldline.replace("'" + matching_list[0] + "'", "'" + elems[i] + "'")
-                                new_elemline_list = old_elemline.split()
-                                new_elemline_list[7] = occupancies[i]
-                                new_elemline_list.append('\n')
-                                new_elemline = ' '.join(new_elemline_list)
-                                cif_string_new += new_elemline
-                        else:
-                            cif_string_new += line + '\n'
-                except:
-                    print 'UNABLE TO PARSE THIS STRUCTURE. ADDING TO LIST OF UNPARSABLE_SDS'
-                    db['unparsable_sds'].insert({'key': doc['key']})
-                    continue
-                try:
-                    db['pauling_file_StructParse'].update({'key': doc['key']}, {
-                        '$set': {'cif_string_new': cif_string_new,
-                                 'structure': CifParser.from_string(cif_string_new).get_structures()[0].as_dict()}},
-                                                          upsert=False)
-                    print 'DONE!'
-                except:
-                    print 'STILL COULD NOT PARSE STRUCTURE. ADDING TO LIST OF UNPARSABLE_SDS'
-                    print(traceback.format_exc())
-                    db['unparsable_sds'].insert({'key': doc['key']})
-                    continue
-                print '#####################################'
-        else:
-            print 'Structure already parsed for ' + doc['key']
-            print doc['structure']
-            print '#####################################'
     print 'Total number of unparsable SD_IDs are: ' + str(db['unparsable_sds'].find().count())
     '''
