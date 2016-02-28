56819d70997c656f842115a63a2879d9522d0772
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-27 16:32:08 -0800

    exporting mongoDB to pandasDF and plotting violing plots

--- analysePauling.py
+++ analysePauling.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     hp_compositions = []
     x = 0
     # for doc in coll.find({'$text': {'$search': 'hp'}}).batch_size(75).limit(100):
-    for doc in coll.find({'metadata._Springer.geninfo.Phase Label(s)': {'$regex': 'hp'}}).batch_size(75).limit(10):
+    for doc in coll.find({'metadata._Springer.geninfo.Phase Label(s)': {'$regex': 'hp'}}).batch_size(75).limit(1000):
         x += 1
         if x % 1000 == 0:
             print x
@@ -33,12 +33,13 @@ if __name__ == '__main__':
             except:
                 print 'HP Density parsing error'
                 density = None
-            newcoll.insert({'key': doc['key'], 'composition': str(comp), 'hp': 'Yes', 'ht': 'No', 'space_group': space_group,
-                            'density': density})
+            newcoll.insert(
+                {'key': doc['key'], 'composition': str(comp), 'hp': 'Yes', 'ht': 'No', 'space_group': space_group,
+                 'density': density, 'property': 1})
     print 'HP DONE!'
     print len(hp_keys)
     z = 0
-    for doc in coll.find().batch_size(75).limit(10):
+    for doc in coll.find().batch_size(75).limit(5000):
         z += 1
         if z % 1000 == 0:
             print z
@@ -47,21 +48,25 @@ if __name__ == '__main__':
             if comp in hp_compositions:
                 print 'MATCH!', doc['key'], comp
                 gs_compositions.append(comp)
-            try:
-                space_group = int(doc['metadata']['_Springer']['geninfo']['Space Group'])
-            except ValueError:
-                print 'HT Space group parsing error'
-                space_group = None
-            try:
-                density = float(doc['metadata']['_Springer']['geninfo']['Density'].split()[2])
-            except:
-                print 'HT Density parsing error'
-                density = None
-            newcoll.insert({'key': doc['key'], 'composition': str(comp), 'hp': 'No', 'ht': 'No', 'space_group': space_group,
-                            'density': density})
+                try:
+                    space_group = int(doc['metadata']['_Springer']['geninfo']['Space Group'])
+                except ValueError:
+                    print 'HT Space group parsing error'
+                    space_group = None
+                try:
+                    density = float(doc['metadata']['_Springer']['geninfo']['Density'].split()[2])
+                except:
+                    print 'HT Density parsing error'
+                    density = None
+                newcoll.insert(
+                    {'key': doc['key'], 'composition': str(comp), 'hp': 'No', 'ht': 'No', 'space_group': space_group,
+                     'density': density, 'property': 1})
     print 'GS DONE!'
     print len(gs_compositions)
     print newcoll
+    cursor = newcoll.find()
+    df = pd.DataFrame(list(cursor))
+    print df.head(20)
     # df['key'] = pd.Series(keys)
     # df['space group'] = pd.Series(space_groups)
     # df['hp'] = pd.Series(hp)
@@ -71,10 +76,10 @@ if __name__ == '__main__':
     # print df.head()
     # print df.shape
     sns.set_style('whitegrid')
-    # sns.violinplot(x='property', y='space group', hue='hp', data=df, palette='muted', split=True, inner='stick')
+    sns.violinplot(x='property', y='space_group', hue='hp', data=df, palette='muted', split=True, inner='stick')
     # sns.violinplot(x='property', y='space group', hue='ht', data=df, palette='muted', split=True, inner='stick')
     # sns.violinplot(x='property', y='density', hue='hp', data=df, palette='muted', split=True, inner='stick')
-    # plt.show()
+    plt.show()
     # tips = sns.load_dataset("tips")
     # sns.violinplot(x="day", y="total_bill", hue="smoker", data=tips, palette="muted", split=True)
     # print tips.head()
