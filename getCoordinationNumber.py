b6c66e14785642496c1f4a67b4904875f86d22a4
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-02-29 17:38:25 -0800

    get coordination numbers by summing up max weights of the Voronoi polyhedra

--- getCoordinationNumber.py
+++ getCoordinationNumber.py
@@ -2,7 +2,6 @@ import pymongo
 from pymatgen.analysis.structure_analyzer import VoronoiCoordFinder
 from pymatgen import Structure
 from pymatgen.io.cif import CifParser
-from utils import *
 
 client = pymongo.MongoClient()
 db = client.springer
@@ -11,9 +10,23 @@ if __name__ == '__main__':
     d = 0
     for doc in db['pauling_file_unique_Parse'].find({'metadata._Springer.geninfo.Standard Formula': 'Li2O'}).limit(1):
         if 'structure' in doc:
+            coordination_numbers = []
             print doc['metadata']['_Springer']['geninfo']['Standard Formula']
             for siteno in range(len(doc['structure']['sites'])):
-                print VoronoiCoordFinder(
-                    CifParser.from_string(doc['cif_string']).get_structures()[0]).get_voronoi_polyhedra(siteno).values()
-            print EffectiveCoordFinder(CifParser.from_string(doc['cif_string']).get_structures()[0]).get_cation_CN()
+                print VoronoiCoordFinder(Structure.from_dict(doc['structure'])).get_coordination_number(siteno)
+                coordination = 0
+                weights = VoronoiCoordFinder(Structure.from_dict(doc['structure'])).get_voronoi_polyhedra(
+                    siteno).values()
+                max_weights_size = len(weights)
+                print weights
+                max_weight = max(weights)
+                for weight in weights:
+                    if weight > 0.80 * max_weight:
+                        coordination += 1
+                print 'Coordination = {}'.format(coordination)
+                print '-----------'
+                coordination_numbers.append(coordination)
+            print coordination_numbers
 
+
+            # print EffectiveCoordFinder(CifParser.from_string(doc['cif_string']).get_structures()[0]).get_cation_CN()
