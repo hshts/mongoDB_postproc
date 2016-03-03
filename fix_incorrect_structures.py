9f53287e2a35db3cb9f34ecdf711c7bbe00807ef
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-03-03 15:58:19 -0800

    catch errors in parsing new cif string, and do not still incorrect/unparsable structures

--- fix_incorrect_structures.py
+++ fix_incorrect_structures.py
@@ -21,33 +21,36 @@ def fix_incorrectlyparsedstructures(cif_string):
     cif_string_new = ''
     for line in cif_lines:
         if ' + ' in line:
-            newline = '#' + line
-            cif_string_new += newline + '\n'
-            matching_list = re.findall(r"\'(.+?)\'", line)
-            elemocc = matching_list[0].split('+')
-            elems = []
-            occupancies = []
-            for i in range(len(elemocc)):
-                occupancies.append('0' + re.findall('\.?\d+', elemocc[i].strip())[1])
-                c = re.findall('\D+', elemocc[i].strip())
-                elems.append(c[1])
-            occ_sum = 0
-            for i in range(len(occupancies)):
-                occ_sum += float(occupancies[i])
-            if occ_sum != 1:
-                sum_exc_last = 0
-                for i in range(len(occupancies) - 1):
-                    sum_exc_last += float(occupancies[i])
-                occupancies[:-1] = str(1 - sum_exc_last)
-            for i in range(len(elems)):
-                oldline = line
-                old_elemline = oldline.replace("'" + matching_list[0] + "'", "'" + elems[i] + "'")
-                new_elemline_list = old_elemline.split()
-                new_elemline_list[0] = elems[i]
-                new_elemline_list[7] = occupancies[i]
-                new_elemline_list.append('\n')
-                new_elemline = ' '.join(new_elemline_list)
-                cif_string_new += new_elemline
+            try:
+                newline = '#' + line
+                cif_string_new += newline + '\n'
+                matching_list = re.findall(r"\'(.+?)\'", line)
+                elemocc = matching_list[0].split('+')
+                elems = []
+                occupancies = []
+                for i in range(len(elemocc)):
+                    occupancies.append('0' + re.findall('\.?\d+', elemocc[i].strip())[1])
+                    c = re.findall('\D+', elemocc[i].strip())
+                    elems.append(c[1])
+                occ_sum = 0
+                for i in range(len(occupancies)):
+                    occ_sum += float(occupancies[i])
+                if occ_sum != 1:
+                    sum_exc_last = 0
+                    for i in range(len(occupancies) - 1):
+                        sum_exc_last += float(occupancies[i])
+                    occupancies[:-1] = str(1 - sum_exc_last)
+                for i in range(len(elems)):
+                    oldline = line
+                    old_elemline = oldline.replace("'" + matching_list[0] + "'", "'" + elems[i] + "'")
+                    new_elemline_list = old_elemline.split()
+                    new_elemline_list[0] = elems[i]
+                    new_elemline_list[7] = occupancies[i]
+                    new_elemline_list.append('\n')
+                    new_elemline = ' '.join(new_elemline_list)
+                    cif_string_new += new_elemline
+            except:
+                break
         else:
             cif_string_new += line + '\n'
     return cif_string_new
@@ -55,7 +58,8 @@ def fix_incorrectlyparsedstructures(cif_string):
 
 if __name__ == '__main__':
     d = 0
-    for incorrect_doc in db['incorrect_structs'].find({'key': 'sd_1011081'}).batch_size(75).sort('_id', pymongo.ASCENDING).skip(d):
+    remove_keys = []
+    for incorrect_doc in db['incorrect_structs'].find().batch_size(75).sort('_id', pymongo.ASCENDING).skip(d):
         d += 1
         print '#######'
         print 'On record # {} and key {}'.format(d, incorrect_doc['key'])
@@ -75,9 +79,23 @@ if __name__ == '__main__':
             print e
             continue
         print 'Formula composition = {}'.format(formula_comp)
+        missing_element = False
         for element in formula_comp:
             if element not in struct_comp:
-                print 'NO MATCH! - Element {} not in structure'.format(element)
-                continue
+                missing_element = True
+                break
+        if missing_element:
+            print 'NO MATCH! - Element {} not in structure'.format(element)
+            continue
         print 'SUCCESS'
+        db['pauling_file_unique_Parse'].update({'key': doc['key']}, {'$set': {'structure': CifParser.from_string(
+        new_cif_string).get_structures()[0].as_dict()}}, upsert=False)
+        db['pauling_file_unique_Parse'].update({'key': doc['key']}, {'$rename': {'cif_string':
+        'metadata._Springer.cif_string_old'}})
+        db['pauling_file_unique_Parse'].update({'key': doc['key']}, {'$set': {'cif_string': new_cif_string}})
+        remove_keys.append(doc['key'])
     print 'FINISHED!'
+    print remove_keys
+    print len(remove_keys)
+    for key in remove_keys:
+        db['incorrect_structs'].remove({'key': key})
