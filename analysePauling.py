9e1a3862ec31ae6f3649cbbc074e675e80b756ae
Saurabh Bajaj <saurabhbajaj2@gmail.com>
2016-03-09 15:02:27 -0800

    use composition from structure instead of from formula

--- analysePauling.py
+++ analysePauling.py
@@ -6,12 +6,13 @@ from pymatgen import Structure, Composition
 
 client = pymongo.MongoClient()
 db = client.springer
-coll = db['pauling_file_unique_Parse']
+coll = db['pauling_file']
 
 
-def insert_states(state, collname):
+def insert_states(state):
     st = state
-    newcoll = db[collname]
+    newcoll = db[state]
+    newcoll.drop()
     keys = []
     compositions = []
     x = 0
@@ -22,8 +23,8 @@ def insert_states(state, collname):
             print x
         if 'structure' in doc:
             keys.append(doc['key'])
-            # comp = Structure.from_dict(doc['structure']).composition
-            comp = Composition(doc['metadata']['_Springer']['geninfo']['Standard Formula'])
+            comp = Structure.from_dict(doc['structure']).composition.reduced_formula
+                # comp = Composition(doc['metadata']['_Springer']['geninfo']['Standard Formula'])
             compositions.append(comp)
             try:
                 space_group = int(doc['metadata']['_Springer']['geninfo']['Space Group'])
@@ -47,42 +48,42 @@ def insert_states(state, collname):
         if y % 1000 == 0:
             print y
         if 'structure' in doc and doc['key'] not in keys:
-            # comp = Structure.from_dict(doc['structure']).composition
-            comp = Composition(doc['metadata']['_Springer']['geninfo']['Standard Formula'])
-            if comp in compositions:
-                print 'MATCH!', doc['key'], comp
-                gs_compositions.append(comp)
-                try:
-                    space_group = int(doc['metadata']['_Springer']['geninfo']['Space Group'])
-                except ValueError:
-                    print 'GS Space group parsing error'
-                    space_group = None
-                try:
-                    density = float(doc['metadata']['_Springer']['geninfo']['Density'].split()[2])
-                except:
-                    print 'GS Density parsing error'
-                    density = None
-                newcoll.insert({'key': doc['key'], 'composition': str(comp), st: 'No', 'space_group': space_group,
-                                'density': density, 'property': 1})
+                comp = Structure.from_dict(doc['structure']).composition.reduced_formula
+                # comp = Composition(doc['metadata']['_Springer']['geninfo']['Standard Formula'])
+                if comp in compositions:
+                    print 'MATCH!', doc['key'], comp
+                    gs_compositions.append(comp)
+                    try:
+                        space_group = int(doc['metadata']['_Springer']['geninfo']['Space Group'])
+                    except ValueError:
+                        print 'GS Space group parsing error'
+                        space_group = None
+                    try:
+                        density = float(doc['metadata']['_Springer']['geninfo']['Density'].split()[2])
+                    except:
+                        print 'GS Density parsing error'
+                        density = None
+                    newcoll.insert({'key': doc['key'], 'composition': str(comp), st: 'No', 'space_group': space_group,
+                                    'density': density, 'property': 1})
     print 'GS DONE!'
     print len(gs_compositions)
 
 
 if __name__ == '__main__':
-    insert_states('hp', 'hp')
-    insert_states('ht', 'ht')
+    insert_states('hp')
+    # insert_states('ht')
     hp_cursor = db['hp'].find()
     hp_df = pd.DataFrame(list(hp_cursor))
     print hp_df.head(20)
-    ht_cursor = db['ht'].find()
-    ht_df = pd.DataFrame(list(ht_cursor))
-    print ht_df.head(20)
+    # ht_cursor = db['ht'].find()
+    # ht_df = pd.DataFrame(list(ht_cursor))
+    # print ht_df.head(20)
     sns.set_style('whitegrid')
     sns.violinplot(x='property', y='space_group', hue='hp', data=hp_df, palette='muted', split=True, inner='stick')
     plt.show()
-    sns.violinplot(x='property', y='space_group', hue='ht', data=ht_df, palette='muted', split=True, inner='stick')
+    # sns.violinplot(x='property', y='space_group', hue='ht', data=ht_df, palette='muted', split=True, inner='stick')
     # sns.violinplot(x='property', y='density', hue='hp', data=df, palette='muted', split=True, inner='stick')
-    plt.show()
+    # plt.show()
     # tips = sns.load_dataset("tips")
     # print tips.head()
     # sns.violinplot(x="day", y="total_bill", hue="smoker", data=tips, palette="muted", split=True)
